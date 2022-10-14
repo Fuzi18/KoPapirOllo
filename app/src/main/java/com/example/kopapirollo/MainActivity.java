3a3bbdc65bb3e63ffe9f53dae3ba24a1be0b0d12
Fuzi18 <fuzesi.t2003@gmail.com>
2022-10-14 09:59:46 +0200

    3.feladat

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kopapirollo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_papir;
     private Button btn_ollo;
     private Random rnd;
+    private int emberEredmeny;
+    private int gepEredmeny;
+    private AlertDialog.Builder builderVege;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,17 +35,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ko.setImageResource(R.drawable.rock);
-                int geptipp=rnd.nextInt(3);
+                int geptipp=rnd.nextInt(2);
                 if (geptipp==0){
                     gep.setImageResource(R.drawable.rock);
                     Toast.makeText(MainActivity.this, "Döntetlen lett", Toast.LENGTH_SHORT).show();
                 } else if(geptipp==1){
                     gep.setImageResource(R.drawable.paper);
                     Toast.makeText(MainActivity.this, "A gép nyert", Toast.LENGTH_SHORT).show();
+                    gepEredmeny++;
                 }
                 else{
                     gep.setImageResource(R.drawable.scissors);
                     Toast.makeText(MainActivity.this, "Te nyertél", Toast.LENGTH_SHORT).show();
+                    emberEredmeny++;
+                }
+                if (emberEredmeny==3){
+                    builderVege.setTitle("Győzelem");
+                    AlertDialog dialog = builderVege.create();
+                    dialog.show();
+                }else if(gepEredmeny==3){
+                    builderVege.setTitle("Vereség");
+                    AlertDialog dialog = builderVege.create();
+                    dialog.show();
+
+
                 }
             }
         });
@@ -49,17 +67,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ko.setImageResource(R.drawable.paper);
-                int geptipp=rnd.nextInt(3);
+                int geptipp=rnd.nextInt(2);
                 if (geptipp==0){
                     gep.setImageResource(R.drawable.rock);
                     Toast.makeText(MainActivity.this, "Te nyertél", Toast.LENGTH_SHORT).show();
                 } else if(geptipp==1){
                     gep.setImageResource(R.drawable.paper);
                     Toast.makeText(MainActivity.this, "Döntetlen lett", Toast.LENGTH_SHORT).show();
+                    gepEredmeny++;
                 }
                 else{
                     gep.setImageResource(R.drawable.scissors);
                     Toast.makeText(MainActivity.this, "A gép nyert", Toast.LENGTH_SHORT).show();
+                    emberEredmeny++;
+                }
+                if (emberEredmeny==3){
+                    builderVege.setTitle("Győzelem");
+                    AlertDialog dialog = builderVege.create();
+                    dialog.show();
+                }else if(gepEredmeny==3){
+                    builderVege.setTitle("Vereség");
+                    AlertDialog dialog = builderVege.create();
+                    dialog.show();
+
+
                 }
             }
         });
@@ -68,17 +99,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ko.setImageResource(R.drawable.scissors);
-                int geptipp=rnd.nextInt(3);
+                int geptipp=rnd.nextInt(2);
                 if (geptipp==0){
                     gep.setImageResource(R.drawable.rock);
                     Toast.makeText(MainActivity.this, "A gép nyert", Toast.LENGTH_SHORT).show();
                 } else if(geptipp==1){
                     gep.setImageResource(R.drawable.paper);
                     Toast.makeText(MainActivity.this, "Te nyertél", Toast.LENGTH_SHORT).show();
+                    gepEredmeny++;
                 }
                 else{
                     gep.setImageResource(R.drawable.scissors);
                     Toast.makeText(MainActivity.this, "Döntetlen lett", Toast.LENGTH_SHORT).show();
+                    emberEredmeny++;
+                }
+                if (emberEredmeny==3){
+                    builderVege.setTitle("Győzelem");
+                    AlertDialog dialog = builderVege.create();
+                    dialog.show();
+                }else if(gepEredmeny==3){
+                    builderVege.setTitle("Vereség");
+                    AlertDialog dialog = builderVege.create();
+                    dialog.show();
+
+
                 }
             }
         });
@@ -92,6 +136,28 @@ public class MainActivity extends AppCompatActivity {
         this.btn_papir=findViewById(R.id.btn_papir);
         this.btn_ollo=findViewById(R.id.btn_ollo);
         rnd=new Random();
+        emberEredmeny=0;
+        gepEredmeny=0;
+        builderVege = new AlertDialog.Builder(MainActivity.this);
+        builderVege.setCancelable(false).setMessage("Szeretne új játékot játszani?")
+                .setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ujJatek();
+                    }
+                })
+                .setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
 
+    }
+    private void ujJatek() {
+        emberEredmeny=0;
+        gepEredmeny=0;
+        ko.setImageResource(R.drawable.rock);
+        gep.setImageResource(R.drawable.rock);
     }
 }
